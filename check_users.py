d1150a73d8d8ad6b613670d6083a434e312a0e15
Sunny Jain <jainsunnyit12@gmail.com>
2026-01-25 16:18:12 +0530

    Chore: Remove unused scripts and sample files

--- check_users.py
+++
@@ -1,7 +0,0 @@
-from prism.database import SessionLocal
-from prism.user_models import User
-db = SessionLocal()
-users = db.query(User).all()
-for u in users:
-    print(f"ID: {u.id}, Email: {u.email}, Name: {u.full_name}, Role: {u.role}")
-db.close()
